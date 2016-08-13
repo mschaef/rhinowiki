b055d3f7bfd01e94bf712a84b396799ac11ccdea
Mike Schaeffer <mike@mschaef.com>
2016-08-13 13:27:44 -0400

    Start building out stanard application handler.

--- src/rhinowiki/core.clj
+++ src/rhinowiki/core.clj
@@ -1,7 +1,82 @@
 (ns rhinowiki.core
-  (:gen-class))
+  (:gen-class)
+  (:use compojure.core
+        [ring.middleware
+         not-modified
+         content-type
+         browser-caching])  
+  (:require [clojure.tools.logging :as log]            
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.file-info :as ring-file-info]
+            [ring.middleware.resource :as ring-resource]
+            [ring.util.response :as ring-response]
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defmacro get-version []
+  ;; Capture compile-time property definition from Lein
+  (System/getProperty "rhinowiki.version"))
+
+(defmacro unless [ condition & body ]
+  `(when (not ~condition)
+     ~@body))
+
+(defn parsable-integer? [ str ]
+  (try
+   (Integer/parseInt str)
+   (catch Exception ex
+     false)))
+
+(defn config-property 
+  ( [ name ] (config-property name nil))
+  ( [ name default ]
+      (let [prop-binding (System/getProperty name)]
+        (if (nil? prop-binding)
+          default
+          (if-let [ int (parsable-integer? prop-binding) ]
+            int
+            prop-binding)))))
+
+(defn add-shutdown-hook [ shutdown-fn ]
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. (fn []
+                               (shutdown-fn)))))
+
+(defn wrap-request-logging [ app ]
+  (fn [req]
+    (log/debug 'REQUEST (:request-method req) (:uri req))
+    (let [resp (app req)]
+      (log/trace 'RESPONSE (:status resp))
+      resp)))
+
+(defn wrap-show-response [ app label ]
+  (fn [req]
+    (let [resp (app req)]
+      (log/trace label (dissoc resp :body))
+      resp)))
+
+(defroutes all-routes
+  ;; user/public-routes
+  (route/resources  (str "/" (get-version)))
+  (route/not-found "Resource Not Found"))
+
+(def handler (-> all-routes
+                 (wrap-content-type)
+                 (wrap-browser-caching {"text/javascript" 360000
+                                        "text/css" 360000})
+                 (wrap-request-logging)
+                 (handler/site)))
+    
+(defn start-webserver [ http-port ]
+  (log/info "Starting Webserver on port" http-port)
+  (let [server (jetty/run-jetty handler { :port http-port :join? false })]
+    (add-shutdown-hook
+     (fn []
+       (log/info "Shutting down webserver")
+       (.stop server)))
+    (.join server)))
+
+(defn -main   [& args]
+  (log/info "Starting Rhinowiki" (get-version))
+  (start-webserver (config-property "http.port" 8080))
+  (log/info "end run."))
