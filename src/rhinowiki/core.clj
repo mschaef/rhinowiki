149badf92556bb138045629f781c8ba7a8c93c3d
Mike Schaeffer <mike@mschaef.com>
2017-10-21 17:36:45 -0400

    Clean up the design of the git read function.

--- src/rhinowiki/core.clj
+++ src/rhinowiki/core.clj
@@ -113,6 +113,22 @@
   (start-webserver (config-property "http.port" 8080))
   (log/info "end run."))
 
+(defn tree-walk-seq [ tree-walk ]
+  (if-let [next (.next tree-walk)]
+    (cons {:path-string (.getPathString tree-walk)
+           :object-id (.getObjectId tree-walk 0)}
+          (lazy-seq (tree-walk-seq tree-walk)))
+    ()))
+
+(defn git-object-string [ repo object-id ]
+  (let [loader (.open repo object-id)]
+    (String. (.getBytes loader) "UTF-8")))
+
+(defn recursive-tree-walk [ repo tree ]
+  (let [tree-walk (org.eclipse.jgit.treewalk.TreeWalk. repo)]
+    (.addTree tree-walk tree)
+    (.setRecursive tree-walk true)
+    tree-walk))
 
 (defn dump-git-markdowns []
   (let [repo (.build (.setWorkTree (org.eclipse.jgit.storage.file.FileRepositoryBuilder.) (java.io.File. ".")))
@@ -120,16 +136,9 @@
     (with-open [walk (org.eclipse.jgit.revwalk.RevWalk. repo)]
       (let [commit (.parseCommit walk (.getObjectId head))
             tree (.parseTree walk (.getId (.getTree commit)))]
-        (with-open [tree-walk (org.eclipse.jgit.treewalk.TreeWalk. repo)]
-          (.addTree tree-walk tree)
-          (.setRecursive tree-walk true)
-          (while (.next tree-walk)
-            (when (.endsWith (.getPathString tree-walk) ".md")
-              (println ">>> " (.getPathString tree-walk))
-              (let [object-id (.getObjectId tree-walk 0)
-                    loader (.open repo object-id)]
-                (.copyTo loader System/out)))))))))
-
-
+        (with-open [tree-walk (recursive-tree-walk repo tree)]
+          (doseq [item (filter #(.endsWith (:path-string %) ".md") (tree-walk-seq tree-walk))]
+            (println ">>> " (:path-string item))
+            (println (git-object-string repo (:object-id item)))))))))
 
 
