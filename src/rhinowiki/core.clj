2adb9995dfcf91eae39c4c98e120922c34b64350
Mike Schaeffer <mike@mschaef.com>
2017-10-21 22:13:44 -0400

    Split git support into its own file.

--- src/rhinowiki/core.clj
+++ src/rhinowiki/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:use compojure.core
         rhinowiki.utils
+        rhinowiki.git
         [ring.middleware not-modified content-type browser-caching])  
   (:require [clojure.tools.logging :as log]            
             [ring.adapter.jetty :as jetty]
@@ -112,33 +113,5 @@
   (log/info "Starting Rhinowiki" (get-version))
   (start-webserver (config-property "http.port" 8080))
   (log/info "end run."))
-
-(defn tree-walk-seq [ tree-walk ]
-  (if-let [next (.next tree-walk)]
-    (cons {:path-string (.getPathString tree-walk)
-           :object-id (.getObjectId tree-walk 0)}
-          (lazy-seq (tree-walk-seq tree-walk)))
-    ()))
-
-(defn git-object-string [ repo object-id ]
-  (let [loader (.open repo object-id)]
-    (String. (.getBytes loader) "UTF-8")))
-
-(defn recursive-tree-walk [ repo tree ]
-  (let [tree-walk (org.eclipse.jgit.treewalk.TreeWalk. repo)]
-    (.addTree tree-walk tree)
-    (.setRecursive tree-walk true)
-    tree-walk))
-
-(defn dump-git-markdowns []
-  (let [repo (.build (.setWorkTree (org.eclipse.jgit.storage.file.FileRepositoryBuilder.) (java.io.File. ".")))
-        head (.getRef repo "refs/heads/master")]
-    (with-open [walk (org.eclipse.jgit.revwalk.RevWalk. repo)]
-      (let [commit (.parseCommit walk (.getObjectId head))
-            tree (.parseTree walk (.getId (.getTree commit)))]
-        (with-open [tree-walk (recursive-tree-walk repo tree)]
-          (doseq [item (filter #(.endsWith (:path-string %) ".md") (tree-walk-seq tree-walk))]
-            (println ">>> " (:path-string item))
-            (println (git-object-string repo (:object-id item)))))))))
 
 
