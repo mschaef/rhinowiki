aa3584fca1f974dbd1c1ae7c9c78ae592cbd4ea6
Mike Schaeffer <mike@mschaef.com>
2018-08-03 00:44:21 -0400

    Add publication date to the RSS feed.

--- src/rhinowiki/rss.clj
+++ src/rhinowiki/rss.clj
@@ -2,6 +2,8 @@
   (:use rhinowiki.utils)
   (:require [clojure.data.xml :as xml]))
 
+(def df-rss-rfc822 (java.text.SimpleDateFormat. "EEE, dd MMM yyyy HH:mm:ss Z"))
+
 (defn rss-blog-email [ blog ]
   (format "%s (%s)" (:blog-email blog) (:blog-author blog)))
 
@@ -11,6 +13,7 @@
                (xml/element "link" {} (:permalink article))
                (xml/element "author" {} (rss-blog-email blog))
                (xml/element "guid" {} (:permalink article))
+               (xml/element "pubDate" {}  (.format df-rss-rfc822 (:date article)))
                (xml/element "description" {} (xml/cdata (:content-html article)))))
 
 (defn rss-blog-feed [ blog articles ]
@@ -22,8 +25,10 @@
                              (xml/element "description" {} (:blog-title blog))
                              (xml/element "managingEditor" {} (rss-blog-email blog))
                              (xml/element "webMaster" {} (rss-blog-email blog))
-                             (xml/element "generator" {} "Rhinowiki")
+                             (xml/element "generator" {} (str "rhinowiki-" (get-version)))
                              (xml/element "language" {} (:blog-language blog))
                              (xml/element "docs" {} "http://blogs.law.harvard.edu/tech/rss")
+                             (xml/element "pubDate" {} (.format df-rss-rfc822 (or (:date (first articles))
+                                                                                    (java.util.Date.))))                             
                              (map #(rss-article-entry blog %) articles)))))
  
