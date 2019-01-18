bef93deb92c353fd5c99ec78b044e1ec06f66c02
Mike Schaeffer <mike@mschaef.com>
2019-01-18 10:52:07 -0500

    Correct empty tag bug.

--- src/rhinowiki/parser.clj
+++ src/rhinowiki/parser.clj
@@ -38,7 +38,7 @@
          :date (or (maybe-parse-metadata-date (first (get-in parsed [ :metadata :date ])))
                    (:file-date raw))
          :sponsor (first (or (get-in parsed [:metadata :sponsor]) [ nil ]))
-         :tags (set (clojure.string/split (first (or (get-in parsed [:metadata :tags]) [""])) #"\s+"))
+         :tags (set (remove empty? (clojure.string/split (first (or (get-in parsed [:metadata :tags]) [""])) #"\s+")))
          :alias (or (get-in parsed [:metadata :alias]) [])})))
 
 
