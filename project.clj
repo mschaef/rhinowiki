f55b59497f3ef9b118ee83ddd9978c216515c467
Mike Schaeffer <mike@mschaef.com>
2020-04-17 15:21:46 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject rhinowiki "0.3.4-SNAPSHOT"
   :description "Rhinowiki Blog Engine"
   :url "http://www.mschaef.com"
-  
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :plugins [[lein-ring "0.9.7"]
             [lein-tar "3.3.0"]]
-  
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "1.0.0"]
                  [ch.qos.logback/logback-classic "1.2.3"]
@@ -18,18 +18,18 @@
                  [danlentz/clj-uuid "0.1.9"]
                  [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "1.10.3-mschaef"]
+                 [markdown-clj "1.10.4"]
                  [cprop "0.1.16"]
                  [org.eclipse.jgit/org.eclipse.jgit "4.4.1.201607150455-r"]
                  [co.deps/ring-etag-middleware "0.2.1"]]
 
   :ring { :handler rhinowiki.core/handler }
-  
+
   :tar {:uberjar true
         :format :tar-gz
         :output-dir "."
         :leading-path "rhinowiki-install"}
-  
+
   :main ^:skip-aot rhinowiki.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
