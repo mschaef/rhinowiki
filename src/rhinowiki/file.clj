3d5652d444c23cba39e90a7a07fd3673bbfe1918
Mike Schaeffer <mike@mschaef.com>
2023-08-26 15:15:14 -0400

    Add a way to exclude a hierarchy within a file backend.

--- src/rhinowiki/file.clj
+++ src/rhinowiki/file.clj
@@ -7,17 +7,20 @@
 
 (defn- load-raw-data-file [ data-file ]
   (log/debug "Loading file: " data-file)
-  (let [ content-raw (binary-slurp data-file) ] 
-    {:file-name (.getName data-file) 
+  (let [ content-raw (binary-slurp data-file) ]
+    {:file-name (.getName data-file)
      :content-raw content-raw
      :id (uuid/v5 file-hash-namespace content-raw)
      :file-date (java.util.Date. (.lastModified data-file))}))
 
-(defn- files-at-root [ root ]
-  (filter #(.isFile %) (file-seq (java.io.File. root))))
+(defn- files-at-root [ root exclude-prefix ]
+  (filter #(and
+            (.isFile %)
+            (not (and exclude-prefix
+                      (.startsWith (.getPath %) exclude-prefix))))
+          (file-seq (java.io.File. root))))
 
-(defn load-data-files [ & {:keys [ article-root ]
-                           :or { article-root ""}} ]
+(defn load-data-files [ & {:keys [ article-root exclude-prefix ]
+                           :or {article-root ""}} ]
   (log/info "Loading data files.")
-  (map load-raw-data-file (files-at-root article-root)))
- 
+  (map load-raw-data-file (files-at-root article-root exclude-prefix)))
