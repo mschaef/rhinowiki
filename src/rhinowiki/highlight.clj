e28a550d5da5f1647e4af00fec8b8f98c2d7d2ca
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2024-02-28 06:38:13 -0500

    Invoke GraalJS directly rather than through ScriptEngineManager.

--- src/rhinowiki/highlight.clj
+++ src/rhinowiki/highlight.clj
@@ -1,5 +1,6 @@
-(ns rhinowiki.highlight)
-
+(ns rhinowiki.highlight
+  (:use playbook.core)
+  (:require [taoensso.timbre :as log]))
 
 (def hljs-languages ["awk" "bash" "basic" "c" "clojure-repl" "clojure"
                      "cpp" "csharp" "css" "diff" "dockerfile" "dos"
@@ -10,18 +11,25 @@
                      "scheme" "scss" "shell" "smalltalk" "sml" "sql"
                      "stata" "swift" "tcl" "typescript" "xml" "yaml"])
 
+(defn make-javascript-context []
+  (-> (org.graalvm.polyglot.Context/newBuilder (into-array String ["js"]))
+      (.allowAllAccess true)
+      (.option "engine.WarnInterpreterOnly" "false")
+      (.build)))
+
+(def highlight-js-code "(function(code, lang) { return hljs.highlight(code, {language: lang}).value; })")
+
 (def highlighter
   (future
-    (let [js (-> (javax.script.ScriptEngineManager.)
-                 (.getEngineByName "graal-js"))]
-      (.eval js (slurp (clojure.java.io/resource "highlight/highlight.js")))
-      (doseq [ lang hljs-languages ]
-        (.eval js (slurp (clojure.java.io/resource (str "highlight/languages/" lang ".js")))))
-      js)))
-
-(def render-code "hljs.highlight(code, {language: lang}).value")
+    (with-exception-barrier "create-highlighter"
+      (let [pg (make-javascript-context)]
+        (.eval pg "js" (slurp (clojure.java.io/resource "highlight/highlight.js")))
+        (doseq [ lang hljs-languages ]
+          (log/debug "Loading highlight language: " lang)
+          (.eval pg "js" (slurp (clojure.java.io/resource (str "highlight/languages/" lang ".js")))))
+        (let [ highlight-js-fn (.eval pg "js" highlight-js-code)]
+          (fn [ code lang ]
+            (.asString (.execute highlight-js-fn (object-array [ code lang ])))))))))
 
 (defn highlight [code lang]
-  (.put @highlighter "code" code)
-  (.put @highlighter "lang" lang)
-  (.eval @highlighter render-code))
+  (@highlighter code lang))
