1eb132f5242241cd177591a7294665291f6b880f
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2024-02-28 20:17:47 -0500

    Switch to a minimized version of Highlight.js 11, which works under Graal. This should reduce the size of the deployment package and simplify startup of the highligher.

--- src/rhinowiki/highlight.clj
+++ src/rhinowiki/highlight.clj
@@ -2,15 +2,6 @@
   (:use playbook.core)
   (:require [taoensso.timbre :as log]))
 
-(def hljs-languages ["awk" "bash" "basic" "c" "clojure-repl" "clojure"
-                     "cpp" "csharp" "css" "diff" "dockerfile" "dos"
-                     "elixir" "erlang" "haskell" "http" "ini" "java"
-                     "javascript" "json" "lisp" "makefile" "markdown"
-                     "nginx" "nix" "objectivec" "ocaml" "pgsql"
-                     "protobuf" "python-repl" "python" "rust" "scala"
-                     "scheme" "scss" "shell" "smalltalk" "sml" "sql"
-                     "stata" "swift" "tcl" "typescript" "xml" "yaml"])
-
 (defn make-javascript-context []
   (-> (org.graalvm.polyglot.Context/newBuilder (into-array String ["js"]))
       (.allowAllAccess true)
@@ -23,10 +14,7 @@
   (future
     (with-exception-barrier "create-highlighter"
       (let [pg (make-javascript-context)]
-        (.eval pg "js" (slurp (clojure.java.io/resource "highlight/highlight.js")))
-        (doseq [ lang hljs-languages ]
-          (log/debug "Loading highlight language: " lang)
-          (.eval pg "js" (slurp (clojure.java.io/resource (str "highlight/languages/" lang ".js")))))
+        (.eval pg "js" (slurp (clojure.java.io/resource "highlight.min.js")))
         (let [ highlight-js-fn (.eval pg "js" highlight-js-code)]
           (fn [ code lang ]
             (.asString (.execute highlight-js-fn (object-array [ code lang ])))))))))
