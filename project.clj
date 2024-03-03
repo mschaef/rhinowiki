e3774402a4b4097aa7655ecd1bc4ec5df6b42a39
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2024-03-03 17:38:38 -0500

    Version 0.3.16

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject rhinowiki "0.3.16-SNAPSHOT"
+(defproject rhinowiki "0.3.16"
   :description "Rhinowiki Blog Engine"
   :url "http://mschaef.com"
 
