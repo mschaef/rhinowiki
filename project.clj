53be873079876e16c4e982df0dec0bfcc86acc38
Mike Schaeffer <mike@mschaef.com>
2025-06-10 07:53:29 -0400

    Version 0.3.19

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject rhinowiki "0.3.19-SNAPSHOT"
+(defproject rhinowiki "0.3.19"
   :description "Rhinowiki Blog Engine"
   :url "http://mschaef.com"
 
