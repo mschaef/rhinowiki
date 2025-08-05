db904e72ee0d826f1b621d4d45c05516f71911c8
Mike Schaeffer <mike@mschaef.com>
2025-08-05 08:20:35 -0400

    Version 0.3.24

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
 ;;
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject rhinowiki "0.3.24-SNAPSHOT"
+(defproject rhinowiki "0.3.24"
   :description "Rhinowiki Blog Engine"
   :url "http://mschaef.com"
 
